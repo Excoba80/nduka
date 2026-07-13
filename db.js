035096abfc6d38a714e250ce21cfb3dce471633e
Obiagwu <steveobi80@gmail.com>
2026-07-13 20:58:53 +0100

    Initial project structure

---
+++ db.js
@@ -0,0 +1,21 @@
+ 
+ 
+   const mysql = require('mysql2');
+   require('dotenv').config();
+   
+
+  // Create a connection pool
+  const pool = mysql.createPool({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD, // This comes from your .env file
+  database: process.env.DB_NAME,     // This comes from your .env file
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+  });
+
+  // Convert the pool to use Promises so we can use "await"
+  const promisePool = pool.promise();
+
+  module.exports = promisePool;
