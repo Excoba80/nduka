349fc8a516b4b9b6ea22303a6064b8af07b648d8
Obiagwu <steveobi80@gmail.com>
2026-07-17 09:06:28 +0100

    Set up authentication foundation with sessions and bcrypt

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
+const session = require("express-session");
 const indexRouter = require("./routes/index");
 const notFound = require("./middleware/notFound");
 const errorHandler = require("./middleware/errorHandler");
@@ -19,6 +20,15 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+
+    cookie: {
+        maxAge: 1000 * 60 * 60
+    }
+}));
 
 app.use(express.static(path.join(__dirname, "public")));
 
